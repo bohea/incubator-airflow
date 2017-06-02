1f709af315784441f1380fcd82a3205f173086cb
bohea <libin375@163.com>
2017-06-02 13:57:47 +0800

    Fix  task instances action bug
    set task instance state or delete task instance from UI will raise exception since execution_date is formated '%Y-%m-%d %H:%M:%S..%f' not '%Y-%m-%d %H:%M:%S

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -2413,7 +2413,7 @@ class TaskInstanceModelView(ModelViewOnly):
             count = len(ids)
             for id in ids:
                 task_id, dag_id, execution_date = id.split(',')
-                execution_date = datetime.strptime(execution_date, '%Y-%m-%d %H:%M:%S')
+                execution_date = datetime.strptime(execution_date, '%Y-%m-%d %H:%M:%S..%f')
                 ti = session.query(TI).filter(TI.task_id == task_id,
                                               TI.dag_id == dag_id,
                                               TI.execution_date == execution_date).one()
@@ -2433,7 +2433,7 @@ class TaskInstanceModelView(ModelViewOnly):
             count = 0
             for id in ids:
                 task_id, dag_id, execution_date = id.split(',')
-                execution_date = datetime.strptime(execution_date, '%Y-%m-%d %H:%M:%S')
+                execution_date = datetime.strptime(execution_date, '%Y-%m-%d %H:%M:%S..%f')
                 count += session.query(TI).filter(TI.task_id == task_id,
                                                   TI.dag_id == dag_id,
                                                   TI.execution_date == execution_date).delete()
